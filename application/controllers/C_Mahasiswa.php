dc2670b5390d00c153270554267ef19b8afd1f4e
arulzkash <arulzkash@gmail.com>
2023-06-06 17:54:32 +0700

    video boostrap 3

--- application/controllers/C_Mahasiswa.php
+++ application/controllers/C_Mahasiswa.php
@@ -14,6 +14,9 @@ class C_Mahasiswa extends CI_Controller
     {
         $data['judul'] = 'Daftar Mahasiswa';
         $data['mahasiswa'] = $this->M_Mahasiswa->getAllMahasiswa();
+        if ($this->input->post('keyword')) {
+            $data['mahasiswa'] = $this->M_Mahasiswa->cariDataMahasiswa();
+        }
         $this->load->view('templates/header', $data);
         $this->load->view('mahasiswa/index', $data);
         $this->load->view('templates/footer');
@@ -38,10 +41,43 @@ class C_Mahasiswa extends CI_Controller
             redirect('c_mahasiswa');
         }
     }
+
     public function hapus($id)
     {
         $this->M_Mahasiswa->hapusDataMahasiswa($id);
         $this->session->set_flashdata('flash2', 'Dihapus');
         redirect('c_mahasiswa');
+    }
+
+    public function detail($id)
+    {
+        $data['judul'] = 'Detail Mahasiswa';
+        $data['mahasiswa'] = $this->M_Mahasiswa->getMahasiswaJurusanById($id);
+
+        $this->load->view('templates/header', $data);
+        $this->load->view('mahasiswa/detail', $data);
+        $this->load->view('templates/footer');
+    }
+
+    public function ubah($id)
+    {
+        $data['judul'] = 'Form Ubah Mahasiswa';
+        $data['jurusan'] = $this->M_Mahasiswa->getJurusan();
+        $data['mahasiswa'] = $this->M_Mahasiswa->getMahasiswaByID($id);
+
+
+        $this->form_validation->set_rules('nama', 'Nama', 'required');
+        $this->form_validation->set_rules('nrp', 'Nrp', 'required|numeric');
+        $this->form_validation->set_rules('email', 'Email', 'required|valid_email');
+
+        if ($this->form_validation->run() == FALSE) {
+            $this->load->view('templates/header', $data);
+            $this->load->view('mahasiswa/ubah', $data);
+            $this->load->view('templates/footer');
+        } else {
+            $this->M_Mahasiswa->ubahDataMahasiswa();
+            $this->session->set_flashdata('flash', 'Diubah');
+            redirect('c_mahasiswa');
+        }
     }
 }
